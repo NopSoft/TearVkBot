b69319eaccb8c8d23de6929a99b61ada0a7f78a6
BulatXam <Khamdbulat@yandex.ru>
2021-12-26 15:57:40 +0300

    feat: add media dir and media files

--- vkbot/config.py
+++ vkbot/config.py
@@ -1,7 +1,11 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables.
+
+BASE_DIR = Path(__file__).parent
+MEDIA_DIR = BASE_DIR / "utils" / "media"
 
 bot_settings = {
     "TOKEN": os.environ.get("BOT_TOKEN"),
